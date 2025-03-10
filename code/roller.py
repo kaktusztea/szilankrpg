9cbd739bc088b8909e7b4e1a0ed4c4a6217e0d73
kaktusztea <kaktusztea@protonmail.com>
2025-03-10 18:48:00 +0100

    roller.py hozzáadva

---
+++ code/roller.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import random
+
+def experiment(dice, throws):
+
+    roll = [ random.randint(1, dice) for _ in range(throws) ]
+    # roll = random.randint(1, dice)
+    return max(roll)
+
+num_experiments = 100000
+num_throws = 4
+num_dice = 100
+
+results = [experiment(num_dice, num_throws) for _ in range(num_experiments)]
+average = sum(results) / num_experiments
+
+print(f"Average result after {num_experiments} experiments: {average}")
+
+
+
