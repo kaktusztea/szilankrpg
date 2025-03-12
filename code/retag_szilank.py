1798a9f01d399fb078acb35b4e4d0785e2000955
kaktusztea <kaktusztea@protonmail.com>
2025-03-12 19:19:19 +0100

    code/retag_szilank.py: frissítése IV

--- code/retag_szilank.py
+++ code/retag_szilank.py
@@ -3,7 +3,7 @@
 
 import git
 import sys
-
+import subprocess
 
 class GitOps:
     def __init__(self, repo_path, work_branch_name):
@@ -26,15 +26,15 @@ class GitOps:
             print("No detached tags found. Exiting.")
             sys.exit(1)
 
-    def is_tag_on_active_branch(self, tag):                                                      # WORKS OK
+    def is_tag_on_active_branch(self, tag):
         active_branch_commit = self.repo.active_branch.commit
         return self.repo.merge_base(active_branch_commit, tag.commit)[0].hexsha == tag.commit.hexsha
 
-    def get_tag_lists(self):                                                                     # WORKS OK
+    def get_tag_lists(self):
         self.tags = sorted(self.repo.tags, key=lambda t: t.commit.committed_date, reverse=False)
         self.tags_detached = [tag for tag in self.tags if not self.is_tag_on_active_branch(tag)]
 
-    def guess_zero_tag(self):                                                                    # WORKS OK
+    def guess_zero_tag(self):
         if not self.is_tag_on_active_branch(self.tags[0]):
             print("First tag is not on active branch. Exiting.")
             return None
@@ -47,24 +47,24 @@ class GitOps:
                 previous_tag = tag
         return None     # if there was no detached tag
 
-    def get_merge_commit_count_between_detached_tag_and_zero(self, tag):                         # WORKS OK
+    def get_merge_commit_count_between_detached_tag_and_zero(self, tag):
         print(f"tag: {tag.name}, merge_commit_count: {self.repo.git.execute(["git", "rev-list", "--count", "--merges", f"{self.zero_tag.commit}..{tag.commit}"]).strip()}")    # DEBUG: remove
         return self.repo.git.execute(["git", "rev-list", "--count", "--merges", f"{self.zero_tag.commit}..{tag.commit}"])
 
     # get commit which is on active branch AND is in distance from zero. Search direction: forward
-    def get_commit_in_distance_from_zero(self, distance, merge_commit_shift=0):                  # WORKS OK
+    def get_commit_in_distance_from_zero(self, distance, merge_commit_shift=0):
         distance = int(distance - merge_commit_shift)       # correction by merge commit counts on detached chain
                                                             # on rewritten master branch this is 0 (they disappear at rewrite)
         zero_commit_hexsha = self.zero_tag.commit
-        output = self.repo.git.execute(["git", "rev-list", "--ancestry-path", "--skip", str(distance), f"{zero_commit_hexsha}..HEAD"])
+        output = self.repo.git.execute(["git", "rev-list", "--ancestry-path", f"{zero_commit_hexsha}..HEAD"])
         target_commit = output.split("\n")[distance*-1].strip()
         return target_commit
 
-    def get_commit_distance(self, commit1, commit2):                                             # WORKS OK
+    def get_commit_distance(self, commit1, commit2):
         output = self.repo.git.execute(["git", "rev-list", "--count", f"{commit1}..{commit2}"])
         return int(output.strip())
 
-    def rename_tag_with_prefix(self, original_tagname):                                          # WORKS OK
+    def rename_tag_with_prefix(self, original_tagname):
         new_tag = f"{self.rename_prefix}{original_tagname}"
         commit = self.repo.commit(original_tagname)
         self.repo.create_tag(new_tag, commit)
@@ -95,7 +95,7 @@ class GitOps:
             # self.repo.git.create_tag(actual_tagname, onbranch_commit)                         # DEBUG: put back, when commits are passing
             # self.repo.git.tag('-d', prefixed_tagname)                                         # DEBUG: put back, when commits are passing
 
-    def dump_tag_infos(self):                                                                   # WORKS OK
+    def dump_tag_infos(self):
         print("Printing all tags...")
         for tag in self.tags:
             print(f"tag: {tag.name}, Commit: {tag.commit}")
@@ -105,5 +105,5 @@ class GitOps:
 
 
 gg = GitOps(repo_path='/repo/github/szilank.code', work_branch_name='master')
-# gg.iterate_and_fix_on_detached_tags()
+gg.iterate_and_fix_on_detached_tags()
 gg.dump_tag_infos()
